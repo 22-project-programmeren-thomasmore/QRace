6fbcbc6694ab211439c277e165643d1b89805784
Aminata <114482391+vloids@users.noreply.github.com>
2023-04-20 19:06:51 +0200

    initial commit

---
+++ Speelgoedrace/src/main/java/com/example/speelgoedrace/SpeelgoedraceApplication.java
@@ -0,0 +1,13 @@
+package com.example.speelgoedrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeelgoedraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeelgoedraceApplication.class, args);
+    }
+
+}
---
+++ Speelgoedrace/src/test/java/com/example/speelgoedrace/SpeelgoedraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speelgoedrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeelgoedraceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
