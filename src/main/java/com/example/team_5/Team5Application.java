dea6a60af3819a7b078a0f6bd0ffe3fb2f6479a2
Aminata <114482391+vloids@users.noreply.github.com>
2023-04-20 21:30:28 +0200

    1

---
+++ src/main/java/com/example/team_5/Team5Application.java
@@ -0,0 +1,13 @@
+package com.example.team_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/team_5/Team5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
