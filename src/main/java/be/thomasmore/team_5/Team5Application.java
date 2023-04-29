8b6c980d19dc82227b372e707c6b35de68b346c4
KaYEET <kai.ekkart@gmail.com>
2023-04-29 12:50:51 +0200

    Fix file structure

--- src/main/java/be/thomasmore/team_5/Team5Application.java
+++ src/main/java/be/thomasmore/team_5/Team5Application.java
@@ -1,4 +1,4 @@
-package com.example.team_5;
+package be.thomasmore.team_5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
