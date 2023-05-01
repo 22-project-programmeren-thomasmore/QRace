e787ae4b726532a2044ffdfafff08a442b024783
gilles20393 <gilles1996@outlook.com>
2023-05-01 17:03:49 +0200

    eerste pagina

--- src/main/java/be/thomasmore/team_5/Team5Application.java
+++ src/main/java/be/thomasmore/team_5/Team5Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Team5Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Team5Application.class, args);
     }
-
 }
