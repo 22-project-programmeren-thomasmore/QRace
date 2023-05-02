4deff29e4860b146cd043b470c4e8cc96af7c073
Soufiane Ahalli <Soufiane.ahalli@student.kdg.be>
2023-05-02 20:23:13 +0200

    navbar + logo

--- src/main/java/com/example/team_5/controller/StartController.java
+++ src/main/java/com/example/team_5/controller/StartController.java
@@ -1,10 +1,19 @@
 package com.example.team_5.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class StartController {
     @GetMapping("/")
-    public String showsStartPage() {
+    public String showStartPage() {
         return "start";
     }
+
+//    @PostMapping("/start")
+//    public String handleStartFormSubmission() {
+//
+//        return "redirect:/game";
+//    }
 }
