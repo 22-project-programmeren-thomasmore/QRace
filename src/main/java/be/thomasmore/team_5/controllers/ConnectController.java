99798cea4d36607bc86d10128695a031c3dce72e
Aminata <114482391+vloids@users.noreply.github.com>
2023-05-02 18:58:17 +0200

    geupdate naar modelandview

--- src/main/java/be/thomasmore/team_5/controllers/ConnectController.java
+++ src/main/java/be/thomasmore/team_5/controllers/ConnectController.java
@@ -2,17 +2,20 @@ package be.thomasmore.team_5.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ConnectController {
 
     @GetMapping("/join")
-    public String join() {
-        return "join";
+    public ModelAndView join() {
+        ModelAndView modelAndView = new ModelAndView("join");
+        return modelAndView;
     }
 
     @GetMapping("/host")
-    public String host() {
-        return "host";
+    public ModelAndView host() {
+        ModelAndView modelAndView = new ModelAndView("host");
+        return modelAndView;
     }
 }
