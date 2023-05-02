204cab68f46b0c1e6584f58e3d42bdf0aea28fac
Aminata <114482391+vloids@users.noreply.github.com>
2023-05-02 18:47:44 +0200

    controller aangemaakt voor de connection pages

---
+++ src/main/java/be/thomasmore/team_5/controllers/ConnectController.java
@@ -0,0 +1,14 @@
+package be.thomasmore.team_5.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConnectController {
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+}
