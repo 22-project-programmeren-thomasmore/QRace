b0630a59fca52c90bfd3a2a866cbdd2223cb15fe
gilles20393 <gilles1996@outlook.com>
2023-05-06 15:01:40 +0200

    # // (testing a feature using openai api) gitlens: generate commit message (experimental)
    Refactor page titles in StartController and start.html templates
    
    Updated the titles in StartController and start.html templates from "QRace" to "qRace" and made the necessary updates in the HTML file accordingly.

--- src/main/java/be/thomasmore/qrace/controllers/StartController.java
+++ src/main/java/be/thomasmore/qrace/controllers/StartController.java
@@ -2,7 +2,6 @@ package be.thomasmore.qrace.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StartController {
