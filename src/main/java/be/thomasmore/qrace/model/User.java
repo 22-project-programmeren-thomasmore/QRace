da974e1508cc2d9dfdcb3c836a717a293169275d
Houssam Aberzak <HOUSSAM.ABERZAK.P@GMAIL.COM>
2023-05-14 18:32:46 +0200

    DATABASE

---
+++ src/main/java/be/thomasmore/qrace/model/User.java
@@ -0,0 +1,52 @@
+package be.thomasmore.qrace.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String username;
+    private String email;
+    private String passwoord;
+
+    public User(String username, String email, String passwoord) {
+        this.username = username;
+        this.email = email;
+        this.passwoord = passwoord;
+    }
+
+    public User() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswoord() {
+        return passwoord;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPasswoord(String passwoord) {
+        this.passwoord = passwoord;
+    }
+}
