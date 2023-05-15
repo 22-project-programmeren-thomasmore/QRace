2edc3c503773c23a8b9d9c51440a2dc11879ae50
gilles20393 <gilles1996@outlook.com>
2023-05-15 19:49:38 +0200

    refactor: Remove unused code, commented out code, and images, add header to start page
    Removed unused code, commented out code, and images from the project to clean up the files. Also added a header to the start page for a better user experience.

--- src/main/java/be/thomasmore/qrace/controllers/NameController.java
+++ src/main/java/be/thomasmore/qrace/controllers/NameController.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 // Annotatie om aan te geven dat deze klasse een Spring Controller is
 @Controller
 // Annotatie om aan te geven welke attributen moeten worden opgeslagen in de sessie
-@SessionAttributes("userName")
+// @SessionAttributes("userName")
 public class NameController {
 
     // Methode om het "userName" attribuut toe te voegen aan het Model
-    @ModelAttribute("userName")
-    public String userName() {
-        return "";
-    }
+    // @ModelAttribute("userName")
+    // public String userName() {
+    //     return "";
+    // }
 
     // Methode om te reageren op een GET-verzoek voor "/name"
     @GetMapping("/name")
@@ -27,11 +27,11 @@ public class NameController {
     }
 
     // Methode om te reageren op een POST-verzoek voor "/name"
-    @PostMapping("/name")
-    public String handleNameSubmit(@RequestParam("name") String name, Model model) {
-        // Voeg de ingevoerde gebruikersnaam toe aan het Model
-        model.addAttribute("userName", name);
-        // Redirect naar de "/host-join" route
-        return "redirect:/host-join";
-    }
+    // @PostMapping("/name")
+    // public String handleNameSubmit(@RequestParam("name") String name, Model model) {
+    //     // Voeg de ingevoerde gebruikersnaam toe aan het Model
+    //     model.addAttribute("userName", name);
+    //     // Redirect naar de "/host-join" route
+    //     return "redirect:/host-join";
+    // }
 }
--- src/main/resources/public/js/translations.js
+++ src/main/resources/public/js/translations.js
@@ -1,31 +1,46 @@
-//all the text for each page in each language
+// All the text for each page in each language
 window.translations = {
   start: {
     nl: {
       info: "Deze pagina doet dit en dat.",
-      title: "Welkom bij de quiz",
-      text: "Deze quiz is gemaakt door studenten van de Hogeschool Rotterdam.",
+      title: "qrace",
+      slogan: "Voltooi de zoektocht & <br> race tegen de mascottes!",
+      intertitle: "Wat is qrace?",
+      article:
+        "Qrace is een zoektocht binnen het Speelgoedmuseum van Mechelen. <br><br> Doorheen het volledige museum zijn QR-codes verstopt. Deze kan je scannen via de qrace-app, de vragen beantwoorden en zoveel mogelijk punten verdienen! <br><br> De snelste speler met de meeste punten wint!",
+      play: "Speel nu!",
     },
     fr: {
       info: "Cette page fait ceci et cela.",
-      title: "Bienvenue au quiz",
-      text: "Ce quiz a été créé par des étudiants de la Hogeschool Rotterdam.",
+      title: "qrace",
+      slogan: "Complétez la quête & <br> affrontez les mascottes !",
+      intertitle: "Qu'est-ce que qrace ?",
+      article:
+        "Qrace est une quête au sein du Musée du Jouet de Malines. <br><br> Des QR-codes sont dissimulés dans tout le musée. Vous pouvez les scanner via l'application qrace, répondre aux questions et accumuler le plus de points possible ! <br><br> Le joueur le plus rapide avec le plus de points gagne !",
+      play: "Jouez maintenant !",
     },
     en: {
       info: "This page does this and that.",
-      title: "Welcome to the quiz",
-      text: "This quiz was made by students from Hogeschool Rotterdam.",
+      title: "qrace",
+      slogan: "Complete the quest & <br> race against the mascots!",
+      intertitle: "What is qrace?",
+      article:
+        "Qrace is a quest within the Toy Museum of Mechelen. <br><br> QR codes are hidden throughout the entire museum. You can scan them using the qrace app, answer the questions, and earn as many points as possible! <br><br> The fastest player with the most points wins!",
+      play: "Play now!",
     },
   },
-  hostJoin: {
+  name: {
     nl: {
-      /* ... */
+      name: "Naam",
+      next: "Volgende",
     },
     fr: {
-      /* ... */
+      name: "nom",
+      next: "prochaine",
     },
     en: {
-      /* ... */
+      name: "name",
+      next: "next",
     },
   },
   // ...
