eda9a7f403e07ad3dcd8a6d716a23d514d7d6505
Aminata <114482391+vloids@users.noreply.github.com>
2023-05-15 18:47:33 +0200

    race controller adjustment pt 2

--- src/main/java/be/thomasmore/qrace/dto/JoinRequest.java
+++ src/main/java/be/thomasmore/qrace/dto/JoinRequest.java
@@ -6,6 +6,6 @@ import lombok.Data;
 @Data
 public class JoinRequest {
     private Player player; //zorg ervoor dat dit een 2de, 3de of 4de player is
-    private String raceId;
+    private String raceID;
 }
 
