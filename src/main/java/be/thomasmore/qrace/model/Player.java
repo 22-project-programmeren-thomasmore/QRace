0e1d48bf49301ce3e34a4aa4fefe0892302aa30f
Aminata <114482391+vloids@users.noreply.github.com>
2023-05-15 17:41:01 +0200

    player class

--- src/main/java/be/thomasmore/qrace/model/Player.java
+++ src/main/java/be/thomasmore/qrace/model/Player.java
@@ -1,4 +1,18 @@
 package be.thomasmore.qrace.model;
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@Builder
 public class Player {
+    @Id
+    private String id;
+    private String name;
+    private Mascot chosenMascot;
+    private int position;
+    private int score;
 }
