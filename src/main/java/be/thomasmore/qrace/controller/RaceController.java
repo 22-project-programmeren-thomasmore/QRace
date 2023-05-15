1dc79df7ba40f254c69ffbcb9473979b9acb917e
Aminata <114482391+vloids@users.noreply.github.com>
2023-05-15 18:42:42 +0200

    race controller adjustment

--- src/main/java/be/thomasmore/qrace/controller/RaceController.java
+++ src/main/java/be/thomasmore/qrace/controller/RaceController.java
@@ -8,19 +8,25 @@ import be.thomasmore.qrace.service.RaceService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @Slf4j
 @AllArgsConstructor
-@RequestMapping("/game")
+@RequestMapping("/race")
 public class RaceController {
     private final RaceService raceService;
-    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @GetMapping({"/", "/lobby"})
+    public String lobby() {
+        return "name";
+    }
+
+    @GetMapping("/host-join")
+    public String hostJoinPage() {
+        return "host-join";
+    }
 
     @PostMapping("/host")
     public ResponseEntity<Race> create(@RequestBody Player hostPlayer) {
@@ -32,5 +38,6 @@ public class RaceController {
     public ResponseEntity<Race> connect(@RequestBody JoinRequest request) throws RaceException {
         log.info("connect request: {}", request);
         return ResponseEntity.ok(raceService.connectToRace(request.getPlayer(), request.getRaceId()));
+    }
     }
 }
