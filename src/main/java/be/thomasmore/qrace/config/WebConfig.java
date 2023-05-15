fe6a8c6f32f686fe26700b2e2e9660e8ba41d7fa
Aminata <114482391+vloids@users.noreply.github.com>
2023-05-15 17:28:19 +0200

    websocket + race controlelr

--- src/main/java/be/thomasmore/qrace/config/WebConfig.java
+++ src/main/java/be/thomasmore/qrace/config/WebConfig.java
@@ -1,4 +1,4 @@
-package be.thomasmore.qrace;
+package be.thomasmore.qrace.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
