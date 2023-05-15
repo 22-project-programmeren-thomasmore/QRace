11b26da7772a9859b30c8d0e2036c2cb0cadd13f
KaYEET <kai.ekkart@gmail.com>
2023-05-16 01:39:37 +0200

    RaceRepository toegevoegd

---
+++ src/main/java/be/thomasmore/qrace/repository/RaceRepository.java
@@ -0,0 +1,7 @@
+package be.thomasmore.qrace.repository;
+
+import be.thomasmore.qrace.model.Race;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RaceRepository extends JpaRepository<Race, Long> {
+}
