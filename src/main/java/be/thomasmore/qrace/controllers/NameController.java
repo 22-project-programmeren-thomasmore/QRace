445aa6acb07ab423e22d7480c65030ee32e76c66
gilles20393 <gilles1996@outlook.com>
2023-05-15 19:54:21 +0200

    namecontroller

--- src/main/java/be/thomasmore/qrace/controllers/NameController.java
+++ src/main/java/be/thomasmore/qrace/controllers/NameController.java
@@ -3,10 +3,11 @@ package be.thomasmore.qrace.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.SessionAttributes;
 
 // Annotatie om aan te geven dat deze klasse een Spring Controller is
 @Controller
@@ -14,24 +15,23 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 // @SessionAttributes("userName")
 public class NameController {
 
-    // Methode om het "userName" attribuut toe te voegen aan het Model
-    // @ModelAttribute("userName")
-    // public String userName() {
-    //     return "";
-    // }
+  // Methode om het "userName" attribuut toe te voegen aan het Model
+  // @ModelAttribute("userName")
+  // public String userName() {
+  //     return "";
+  // }
 
-    // Methode om te reageren op een GET-verzoek voor "/name"
-    @GetMapping("/name")
-    public String showNameForm() {
-        return "name";
-    }
-
-    // Methode om te reageren op een POST-verzoek voor "/name"
-    // @PostMapping("/name")
-    // public String handleNameSubmit(@RequestParam("name") String name, Model model) {
-    //     // Voeg de ingevoerde gebruikersnaam toe aan het Model
-    //     model.addAttribute("userName", name);
-    //     // Redirect naar de "/host-join" route
-    //     return "redirect:/host-join";
-    // }
+  // Methode om te reageren op een GET-verzoek voor "/name"
+  @GetMapping("/name")
+  public String showNameForm() {
+    return "name";
+  }
+  // Methode om te reageren op een POST-verzoek voor "/name"
+  // @PostMapping("/name")
+  // public String handleNameSubmit(@RequestParam("name") String name, Model model) {
+  //     // Voeg de ingevoerde gebruikersnaam toe aan het Model
+  //     model.addAttribute("userName", name);
+  //     // Redirect naar de "/host-join" route
+  //     return "redirect:/host-join";
+  // }
 }
