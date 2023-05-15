76fb0458b4fda2b42268acd8f46ca679b2aea06d
gilles20393 <gilles1996@outlook.com>
2023-05-15 19:47:57 +0200

    Add translations data, update bootstrap and add language selection feature
    The commit adds a new translations file providing the text to each page in different languages. Updates bootstrap and replaces the bootstrap JS with the updated version. The "help-language" file is deleted, and the language selection feature is added to the "header" file.

---
+++ src/main/resources/public/js/translations.js
@@ -0,0 +1,32 @@
+//all the text for each page in each language
+window.translations = {
+  start: {
+    nl: {
+      info: "Deze pagina doet dit en dat.",
+      title: "Welkom bij de quiz",
+      text: "Deze quiz is gemaakt door studenten van de Hogeschool Rotterdam.",
+    },
+    fr: {
+      info: "Cette page fait ceci et cela.",
+      title: "Bienvenue au quiz",
+      text: "Ce quiz a été créé par des étudiants de la Hogeschool Rotterdam.",
+    },
+    en: {
+      info: "This page does this and that.",
+      title: "Welcome to the quiz",
+      text: "This quiz was made by students from Hogeschool Rotterdam.",
+    },
+  },
+  hostJoin: {
+    nl: {
+      /* ... */
+    },
+    fr: {
+      /* ... */
+    },
+    en: {
+      /* ... */
+    },
+  },
+  // ...
+};
