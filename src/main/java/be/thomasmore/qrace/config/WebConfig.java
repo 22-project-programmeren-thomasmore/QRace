08f986172f4a91ffdbe9b603d2e152a5acd8e825
KaYEET <kai.ekkart@gmail.com>
2023-05-17 03:13:53 +0200

    Bug gefixt, index aangepast

--- src/main/java/be/thomasmore/qrace/config/WebConfig.java
+++ src/main/java/be/thomasmore/qrace/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry
-      .addResourceHandler("/public/**")
-      .addResourceLocations("classpath:/public/");
+            .addResourceHandler("/public/***")
+            .addResourceLocations("classpath:/public/");
+
   }
 }
--- src/main/java/be/thomasmore/qrace/model/Mascot.java
+++ src/main/java/be/thomasmore/qrace/model/Mascot.java
@@ -1,15 +1,25 @@
 package be.thomasmore.qrace.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+@Entity
 @AllArgsConstructor
 @Data
 @Builder
 // de mascottes zijn de verschillende characters binnen QRace
 public class Mascot {
+    @Id
+    private String MascotID;
+
     private String name;
     private String image;
     private String description;
+
+    public Mascot() {
+
+    }
 }
