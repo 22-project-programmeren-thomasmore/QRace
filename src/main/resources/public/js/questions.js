f137e5156406e17896754ce6d72205dd9519cb2f
gilles20393 <gilles1996@outlook.com>
2023-05-26 19:57:44 +0200

    scan shows random question from the groupParameter

--- src/main/resources/public/js/questions.js
+++ src/main/resources/public/js/questions.js
@@ -1,54 +1,75 @@
-// Fetch all questions from the server
-fetch('/api/questions')
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Failed to fetch questions");
-    }
-  })
-  .then((questions) => {
-    console.log(questions);
-    // Process the questions and update the UI
-    displayQuestions(questions);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-    // Handle error scenario
+// Get a random question from the available non-archived questions in the specified group and language
+function getRandomQuestion(group, language, questions) {
+    // Filter the questions based on the group, archived status, and language
+    const filteredQuestions = questions.filter(
+      (question) =>
+        question.groupParameter === group &&
+        !question.archived &&
+        question.language === language
+    );
+  
+    // Select a random question from the filtered questions
+    const randomIndex = Math.floor(Math.random() * filteredQuestions.length);
+    return filteredQuestions[randomIndex];
+  }
+  
+  // Function to handle question selection and UI update
+  function selectRandomQuestion(groupParameter) {
+    // Fetch all questions from the server
+    fetch('/api/questions')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to fetch questions");
+        }
+      })
+      .then((questions) => {
+        // Get a random question from the available non-archived questions in the specified group and language
+        const randomQuestion = getRandomQuestion(groupParameter, 'nl', questions);
+  
+        // Display the question on the page
+        const questionTextElement = document.getElementById('questionText');
+        questionTextElement.textContent = randomQuestion.questionText;
+  
+        // Display the answers on the page
+        const answerContainer = document.getElementById('answerContainer');
+        answerContainer.innerHTML = ''; // Clear previous answers
+  
+        // Loop through the answers and add them to the answer container
+        const answerKeys = ['answer1', 'answer2', 'answer3', 'answer4'];
+        for (const key of answerKeys) {
+          const answer = randomQuestion[key];
+          if (answer) {
+            const answerElement = document.createElement('p');
+            answerElement.textContent = answer;
+            answerContainer.appendChild(answerElement);
+          }
+        }
+  
+        // Show the question and answers container
+        const questionContainer = document.getElementById('questionContainer');
+        questionContainer.style.display = 'block';
+  
+        // Handle the submit answer button click event
+        const submitAnswerBtn = document.getElementById('submitAnswerBtn');
+        submitAnswerBtn.addEventListener('click', () => {
+          const selectedAnswer = document.querySelector('input[name="answer"]:checked').value;
+          // TODO: Handle the selected answer
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // Handle error scenario
+      });
+  }
+  
+  // Event listener for the custom "exportData" event
+  document.addEventListener("exportData", (event) => {
+    const groupParameter = event.detail.groupParameter; // Adjust the property name accordingly
+    console.log('groupParameter: ', groupParameter);
+  
+    // Call the function to select a random question and update the UI
+    selectRandomQuestion(groupParameter);
   });
-
-
-
-function displayQuestions(questions) {
-  // Clear the previous questions
-  questionContainer.innerHTML = "";
-
-  // Loop through the questions and create HTML elements to display them
-  questions.forEach((question) => {
-    // Create a container for the question
-    const questionElement = document.createElement("div");
-    questionElement.classList.add("question");
-
-    // Create elements for the question text, answer options, and correct answer
-    const questionTextElement = document.createElement("p");
-    questionTextElement.textContent = question.questionText;
-
-    const answerOptionsElement = document.createElement("ul");
-    question.answerOptions.forEach((answerOption) => {
-      const li = document.createElement("li");
-      li.textContent = answerOption;
-      answerOptionsElement.appendChild(li);
-    });
-
-    const correctAnswerElement = document.createElement("p");
-    correctAnswerElement.textContent = question.correctAnswer;
-
-    // Append the elements to the question container
-    questionElement.appendChild(questionTextElement);
-    questionElement.appendChild(answerOptionsElement);
-    questionElement.appendChild(correctAnswerElement);
-
-    // Append the question container to the main container
-    questionContainer.appendChild(questionElement);
-  });
-}
+  
