cc6069afa727e413c9407ba982996e6f80618b7f
gilles20393 <gilles1996@outlook.com>
2023-05-27 02:21:21 +0200

    Refactor the selectRandomQuestion function to reuse the getRandomQuestion function and handle scenarios when no questions are available. Add functionality to handle a second chance to answer each question.

--- src/main/resources/public/js/questions.js
+++ src/main/resources/public/js/questions.js
@@ -1,3 +1,88 @@
+// Event listener for the custom "exportData" event
+document.addEventListener("exportData", (event) => {
+  const groupParameter = event.detail; // Adjust the property name accordingly
+  // console.log('groupParameter: ', groupParameter);
+
+  // Call the function to select a random question and update the UI
+  selectRandomQuestion(groupParameter);
+});
+
+// Function to handle question selection and UI update
+function selectRandomQuestion(groupParameter) {
+  // Fetch all questions from the server
+  fetch('/api/questions')
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Failed to fetch questions");
+      }
+    })
+    .then((questions) => {
+      // Check if there's a group in session storage
+      const { group } = getAnsweredQuestion();
+      if (group && group === groupParameter) {
+        // If the group in session storage is the same as the current group, don't select a question
+        console.error("You have already answered a question from this group");
+        // Here you could show an error message to the user
+        return;
+      }
+
+      // Check if there's a question ID in session storage
+      const { questionId } = getAnsweredQuestion();
+      if (questionId) {
+        // Filter out the question with the ID in session storage
+        questions = questions.filter((question) => question.id !== questionId);
+      }
+
+      // Get a random question from the available non-archived questions in the specified group and language
+      const randomQuestion = getRandomQuestion(groupParameter, questions);
+
+      // Check if a random question was successfully selected
+      if (randomQuestion) {
+        // Store the answered question ID and attempts in session storage
+        storeAnsweredQuestion(randomQuestion.id, 0, groupParameter);
+
+
+        // Display the question on the page
+        const questionTextElement = document.getElementById('questionText');
+        questionTextElement.textContent = randomQuestion.questionText;
+
+        // Display the answers on the page
+        const answerContainer = document.getElementById('answerContainer');
+        answerContainer.innerHTML = ''; // Clear previous answers
+
+        // Loop through the answers and add them to the answer container
+        const answerKeys = ['answer1', 'answer2', 'answer3', 'answer4'];
+        for (const key of answerKeys) {
+          const answer = randomQuestion[key];
+          if (answer) {
+            const answerCard = document.createElement('div');
+            answerCard.id = key;
+            answerCard.className = 'answer-card';
+            answerCard.textContent = answer;
+            answerCard.dataset.correctAnswer = randomQuestion.correctAnswer;
+            answerCard.addEventListener('click', () => {
+              handleAnswerClick(key);
+            });
+            answerContainer.appendChild(answerCard);
+          }
+        }
+
+        // Show the question and answers container
+        const questionContainer = document.getElementById('questionContainer');
+        questionContainer.style.display = 'block';
+      } else {
+        // Handle the case where no question was selected
+        console.error("No questions available for the selected group and language");
+        // Here you could show an error message to the user
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // Handle error scenario
+    });
+}
 // Get a random question from the available non-archived questions in the specified group and language
 function getRandomQuestion(group, questions) {
   // Get the language from the cookies
@@ -16,57 +101,6 @@ function getRandomQuestion(group, questions) {
   return filteredQuestions[randomIndex];
 }
 
-
-// Function to handle question selection and UI update
-function selectRandomQuestion(groupParameter) {
-  // Fetch all questions from the server
-  fetch('/api/questions')
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Failed to fetch questions");
-      }
-    })
-    .then((questions) => {
-      // Get a random question from the available non-archived questions in the specified group and language
-      const randomQuestion = getRandomQuestion(groupParameter, questions);
-
-      // Display the question on the page
-      const questionTextElement = document.getElementById('questionText');
-      questionTextElement.textContent = randomQuestion.questionText;
-
-      // Display the answers on the page
-      const answerContainer = document.getElementById('answerContainer');
-      answerContainer.innerHTML = ''; // Clear previous answers
-
-      // Loop through the answers and add them to the answer container
-      const answerKeys = ['answer1', 'answer2', 'answer3', 'answer4'];
-      for (const key of answerKeys) {
-        const answer = randomQuestion[key];
-        if (answer) {
-          const answerCard = document.createElement('div');
-          answerCard.id = key;
-          answerCard.className = 'answer-card';
-          answerCard.textContent = answer;
-          answerCard.dataset.correctAnswer = randomQuestion.correctAnswer;
-          answerCard.addEventListener('click', () => {
-            handleAnswerClick(key);
-          });
-          answerContainer.appendChild(answerCard);
-        }
-      }
-
-      // Show the question and answers container
-      const questionContainer = document.getElementById('questionContainer');
-      questionContainer.style.display = 'block';
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      // Handle error scenario
-    });
-}
-
 // Handle the answer click event
 function handleAnswerClick(selectedAnswer) {
   const selectedCard = document.getElementById(selectedAnswer);
@@ -81,6 +115,16 @@ function handleAnswerClick(selectedAnswer) {
   } else {
     selectedCard.classList.add('incorrect');
     console.log("Incorrect answer selected"); // Log to console
+
+    // Increment attempts and give a second chance if attempts is less than 2
+    const { questionId, attempts, group } = getAnsweredQuestion();
+    const newAttempts = Number(attempts) + 1;
+    storeAnsweredQuestion(questionId, newAttempts, group);
+    if (newAttempts < 2) {
+      selectRandomQuestion(group);
+    } else {
+      clearSessionStorage();
+    }
   }
 
   // Hide all answer cards
@@ -94,12 +138,22 @@ function handleAnswerClick(selectedAnswer) {
 
 }
 
+// Store the answered question ID, attempts, and group
+function storeAnsweredQuestion(questionId, attempts, group) {
+  sessionStorage.setItem('answeredQuestionId', questionId);
+  sessionStorage.setItem('attempts', attempts);
+  sessionStorage.setItem('answeredGroup', group);
+}
 
-// Event listener for the custom "exportData" event
-document.addEventListener("exportData", (event) => {
-  const groupParameter = event.detail.groupParameter; // Adjust the property name accordingly
-  // console.log('groupParameter: ', groupParameter);
+// Retrieve the answered question ID, attempts, and group
+function getAnsweredQuestion() {
+  const questionId = sessionStorage.getItem('answeredQuestionId');
+  const attempts = sessionStorage.getItem('attempts');
+  const group = sessionStorage.getItem('answeredGroup');
+  return { questionId, attempts, group };
+}
 
-  // Call the function to select a random question and update the UI
-  selectRandomQuestion(groupParameter);
-});
+// Clear the session storage
+function clearSessionStorage() {
+  sessionStorage.clear();
+}
