3aafad5483ac3017e799714c14b2b9167a9e7b5e
Aminata <114482391+vloids@users.noreply.github.com>
2023-05-31 07:47:43 +0200

    added error responses to code

--- src/main/java/be/thomasmore/qrace/config/websocket/WebSocketController.java
+++ src/main/java/be/thomasmore/qrace/config/websocket/WebSocketController.java
@@ -12,13 +12,11 @@ import java.util.Map;
 public class WebSocketController {
     private Map<String, WebSocketSession> playerSessions = new HashMap<>();
 
-    private void sendGameUpdateToUser(WebSocketSession userSession, RaceUpdate raceUpdate) {
-        try {
-            if (userSession != null && userSession.isOpen()) {
-                userSession.sendMessage(new TextMessage(raceUpdate.toJson()));
-            }
-        } catch (IOException e) {
-            // Handle the exception
+    private void sendGameUpdateToUser(WebSocketSession userSession, RaceUpdate raceUpdate) throws IOException {
+        if (userSession != null && userSession.isOpen()) {
+            userSession.sendMessage(new TextMessage(raceUpdate.toJson()));
+        } else {
+            throw new IOException("User session is closed or null.");
         }
     }
 
@@ -31,7 +29,13 @@ public class WebSocketController {
         // Retrieve the specific user's WebSocket session based on the session ID
         WebSocketSession userSession = playerSessions.get(sessionId);
 
-        // Send the game update to the specific user
-        sendGameUpdateToUser(userSession, raceUpdate);
+        try {
+            // Send the game update to the specific user
+            sendGameUpdateToUser(userSession, raceUpdate);
+        } catch (IOException e) {
+            // Handle the exception and return an error response to the client
+            e.printStackTrace();
+            // You can return an error response to the client or perform other error handling actions
+        }
     }
 }
