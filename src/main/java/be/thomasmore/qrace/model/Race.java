eb84d6faa144a767535dab37f921d7b96f74000b
Aminata <114482391+vloids@users.noreply.github.com>
2023-05-31 07:43:46 +0200

    race updated to get questions from database

--- src/main/java/be/thomasmore/qrace/model/Race.java
+++ src/main/java/be/thomasmore/qrace/model/Race.java
@@ -1,13 +1,13 @@
 package be.thomasmore.qrace.model;
 
 import jakarta.persistence.*;
-import be.thomasmore.qrace.model.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 @Entity
@@ -53,49 +53,24 @@ public class Race {
 
     public void startRace() {
         performQRCodeScanning();
-        // Set initial values for the race
-        totalQuestions = 5;
-        maxAttempts = 2;
-        remainingAttempts = maxAttempts;
-
-        // Start the race
-        status = RaceStatusEnum.IN_PROGRESS;
-
-        // Display the first question
-        nextQuestion();
+        initializeRace();
+        startNextQuestion();
     }
 
-    public void nextQuestion() {
-        // Check if there are remaining questions
-        if (totalQuestions > 0) {
-            // Display the question
-            String question = getQuestionFromDatabase(); // Replace with your logic to fetch the question
-            System.out.println("Question: " + question);
-            totalQuestions--;
+    public void startNextQuestion() {
+        if (areQuestionsRemaining()) {
+            displayQuestion();
+            decrementTotalQuestions();
         } else {
-            // All questions have been answered
             finishRace();
         }
     }
 
-    public void answerQuestion(String answer) {
+    public void submitAnswer(String answer) {
         if (isAnswerCorrect(answer)) {
-            // Correct answer
-            playerScore += remainingAttempts == maxAttempts ? 3 : 1;
-            System.out.println("Correct answer! Score: " + playerScore);
-            remainingAttempts = maxAttempts;
+            handleCorrectAnswer();
         } else {
-            // Wrong answer
-            remainingAttempts--;
-
-            if (remainingAttempts == 0) {
-                // No more attempts left
-                System.out.println("Wrong answer! No more attempts. Score: " + playerScore);
-                nextQuestion();
-            } else {
-                // More attempts remaining
-                System.out.println("Wrong answer! Try again. Remaining attempts: " + remainingAttempts);
-            }
+            handleWrongAnswer();
         }
     }
 
@@ -111,13 +86,24 @@ public class Race {
         setScannedQRCode(scannedValue);
     }
 
-    public void setScannedQRCode(String scannedQRCode) {
-        this.scannedQRCode = scannedQRCode;
+    private void initializeRace() {
+        totalQuestions = 5;
+        maxAttempts = 2;
+        remainingAttempts = maxAttempts;
+        status = RaceStatusEnum.IN_PROGRESS;
     }
 
-    private String getQuestionFromDatabase() {
-        // Replace with your logic to fetch the question from the database
-        return "Sample question";
+    private void displayQuestion() {
+        String question = getQuestionFromDatabase(); // Replace with your logic to fetch the question
+        System.out.println("Question: " + question);
+    }
+
+    private void decrementTotalQuestions() {
+        totalQuestions--;
+    }
+
+    private boolean areQuestionsRemaining() {
+        return totalQuestions > 0;
     }
 
     private boolean isAnswerCorrect(String answer) {
@@ -125,8 +111,49 @@ public class Race {
         return answer.equalsIgnoreCase("sample answer");
     }
 
+    private void handleCorrectAnswer() {
+        playerScore += remainingAttempts == maxAttempts ? 3 : 1;
+        System.out.println("Correct answer! Score: " + playerScore);
+        remainingAttempts = maxAttempts;
+        startNextQuestion();
+    }
+
+    private void handleWrongAnswer() {
+        remainingAttempts--;
+
+        if (remainingAttempts == 0) {
+            System.out.println("Wrong answer! No more attempts. Score: " + playerScore);
+            startNextQuestion();
+        } else {
+            System.out.println("Wrong answer! Try again. Remaining attempts: " + remainingAttempts);
+        }
+    }
+
     private void finishRace() {
         status = RaceStatusEnum.FINISHED;
         System.out.println("Race finished! Final score: " + playerScore);
     }
+
+    private void setScannedQRCode(String scannedQRCode) {
+        this.scannedQRCode = scannedQRCode;
+    }
+
+    private String getQuestionFromDatabase() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("qrace");
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery<Question> query = em.createQuery("SELECT q FROM Question q", Question.class);
+            List<Question> questions = query.getResultList();
+
+            // Get a random question from the list
+            Random random = new Random();
+            Question randomQuestion = questions.get(random.nextInt(questions.size()));
+
+            return randomQuestion.getQuestionText();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+
 }
